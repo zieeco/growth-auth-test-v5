bf97468464d116303bbd13c77952fcf88ebff197
zieeco <zieecosam@gmail.com>
2024-08-08 17:50:51 +0100

    chore: Initial project structure

---
+++ src/auth.ts
@@ -0,0 +1,9 @@
+import { MongoDBAdapter} from '@auth/mongodb-adapter';
+import NextAuth from 'next-auth';
+import { Adapter} from 'next-auth/adapters';
+import client from './lib/mongo';
+
+export const {handlers, auth, signIn, signOut} = NextAuth({
+	adapter: MongoDBAdapter(client) as Adapter,
+	providers: [],
+});
