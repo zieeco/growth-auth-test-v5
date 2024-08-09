8b76450b053ae17f6bfce4f5a2eb9e21e27fad75
zieeco <zieecosam@gmail.com>
2024-08-09 12:02:52 +0100

    style: format using prettier, and ignore the .prettierrc file

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -1,35 +1,35 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
-import { MongoClient, ServerApiVersion} from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
-	throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
 const uri = process.env.MONGODB_URI;
 const options = {
-	serverApi: {
-		version: ServerApiVersion.v1,
-		strict: true,
-		deprecationErrors: true,
-	},
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 };
 
 let client: MongoClient;
 
 if (process.env.NODE_ENV === 'development') {
-	// In development mode, use a global variable so that the value
-	// is preserved across module reloads caused by HMR (Hot Module Replacement).
-	let globalWithMongo = global as typeof globalThis & {
-		_mongoClient?: MongoClient;
-	};
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClient?: MongoClient;
+  };
 
-	if (!globalWithMongo._mongoClient) {
-		globalWithMongo._mongoClient = new MongoClient(uri, options);
-	}
-	client = globalWithMongo._mongoClient;
+  if (!globalWithMongo._mongoClient) {
+    globalWithMongo._mongoClient = new MongoClient(uri, options);
+  }
+  client = globalWithMongo._mongoClient;
 } else {
-	// In production mode, it's best to not use a global variable.
-	client = new MongoClient(uri, options);
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
 }
 
 // Export a module-scoped MongoClient. By doing this in a
