5033cfbd4b636980b9f94dae26bab4a0c1c78084
zieeco <zieecosam@gmail.com>
2024-08-09 08:38:00 +0100

    refacto: update variabe name

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -14,24 +14,29 @@ const options = {
 	},
 };
 
-let client: MongoClient;
+let client;
+let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === 'development') {
 	// In development mode, use a global variable so that the value
 	// is preserved across module reloads caused by HMR (Hot Module Replacement).
-	let globalWithMongo = global as typeof globalThis & {
-		_mongoClient?: MongoClient;
+	const globalWithMongo = global as typeof globalThis & {
+		_mongoClientPromise?: Promise<MongoClient>;
 	};
 
-	if (!globalWithMongo._mongoClient) {
-		globalWithMongo._mongoClient = new MongoClient(uri, options);
+	if (!globalWithMongo._mongoClientPromise) {
+		client = new MongoClient(uri, options);
+
+		globalWithMongo._mongoClientPromise = client.connect();
 	}
-	client = globalWithMongo._mongoClient;
+	clientPromise = globalWithMongo._mongoClientPromise;
+	// client = globalWithMongo._mongoClient;
 } else {
 	// In production mode, it's best to not use a global variable.
 	client = new MongoClient(uri, options);
+	clientPromise = client.connect();
 }
 
 // Export a module-scoped MongoClient. By doing this in a
 // separate module, the client can be shared across functions.
-export default client;
+export default clientPromise;
