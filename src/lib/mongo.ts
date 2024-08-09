ddf94ef7351c078de38d164c70e8f432a76da47b
zieeco <zieecosam@gmail.com>
2024-08-09 09:39:10 +0100

    chore(dep): add more dependencies

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -40,3 +40,4 @@ if (process.env.NODE_ENV === 'development') {
 // Export a module-scoped MongoClient. By doing this in a
 // separate module, the client can be shared across functions.
 export default clientPromise;
+ 
