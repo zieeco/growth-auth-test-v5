b66682d4baf440f2e3ec059ede2050b396c0441d
zieeco <zieecosam@gmail.com>
2024-08-10 23:29:21 +0100

    feat: improve User models fields

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,14 @@
-import { Model, model, models, Document, Schema } from 'mongoose';
+import { Model, model, models, Document, Schema, Types } from 'mongoose';
 interface UserDocument extends Document {
   name: string;
   email: string;
+  emailVerified?: Date;
   password?: string;
   role?: string;
   image?: string;
   authProviderId?: string;
+  accounts?: Types.ObjectId[];
+  sessions?: Types.ObjectId[];
   createdAt: Date;
   updatedAt: Date;
 }
@@ -14,15 +17,19 @@ const userSchema: Schema<UserDocument> = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    emailVerified: { type: Date, default: null },
     password: { type: String, select: false },
     role: { type: String, default: 'user' },
     image: { type: String },
     authProviderId: { type: String },
+    accounts: [{ type: Schema.Types.ObjectId, ref: 'Account' }],
+    sessions: [{ type: Schema.Types.ObjectId, ref: 'Session' }],
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
   },
   {
     timestamps: true,
+    collection: 'users',
   },
 );
 
