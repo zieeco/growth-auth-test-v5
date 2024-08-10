f918848f4400d34f7104fa1f62a3e7d0c146e900
zieeco <zieecosam@gmail.com>
2024-08-10 08:48:15 +0100

    refactor: refactor to make simpler

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -1,5 +1,8 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import {
+  MongoClient,
+  //  ServerApiVersion
+} from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
@@ -7,31 +10,31 @@ if (!process.env.MONGODB_URI) {
 
 const uri = process.env.MONGODB_URI;
 const options = {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+  // serverApi: {
+  //   version: ServerApiVersion.v1,
+  //   strict: true,
+  //   deprecationErrors: true,
+  // },
 };
 
 let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  // In development mode, use a global variable so the client can be reused across module reloads
   let globalWithMongo = global as typeof globalThis & {
-    _mongoClient?: MongoClient;
+    _mongoClientPromise?: Promise<MongoClient>;
   };
 
-  if (!globalWithMongo._mongoClient) {
-    globalWithMongo._mongoClient = new MongoClient(uri, options);
+  if (!globalWithMongo._mongoClientPromise) {
+    client = new MongoClient(uri, {});
+    globalWithMongo._mongoClientPromise = client.connect();
   }
-  client = globalWithMongo._mongoClient;
+  clientPromise = globalWithMongo._mongoClientPromise;
 } else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
+  // In production mode, create a new MongoClient instance and connect
+  client = new MongoClient(uri, {});
+  clientPromise = client.connect();
 }
 
-// Export a module-scoped MongoClient. By doing this in a
-// separate module, the client can be shared across functions.
-export default client;
+export default clientPromise;
