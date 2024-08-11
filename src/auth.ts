379317ce21f972b9c6622ac55cad66eec44f6e91
zieeco <zieecosam@gmail.com>
2024-08-11 21:32:58 +0100

    fix: temporal fix to the next-auth@beta trustHost issue

--- src/auth.ts
+++ src/auth.ts
@@ -7,6 +7,7 @@ import GitHub from 'next-auth/providers/github';
 import Twitter from 'next-auth/providers/twitter';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true, // Temporal fix the next-auth beta error. This issue is under fixing by next-auth-beta.
   theme: {
     logo: '/favicon.png',
   },
